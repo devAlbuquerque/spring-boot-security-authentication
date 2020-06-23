3cd35b1fdc885cc3b7d35bb15b99d6e481607202
Antonio <antonio.erismar@gmail.com>
2020-06-23 15:46:04 -0300

    Adicionando troca de bases de dados através de perfil de escolha no application.properties

--- src/main/java/com/eventos/springapp/data/DataConfiguration.java
+++ src/main/java/com/eventos/springapp/data/DataConfiguration.java
@@ -2,38 +2,94 @@ package com.eventos.springapp.data;
 
 import javax.sql.DataSource;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
-@Profile(value = { "dev" })
 @Configuration
+@ConfigurationProperties("datasource.bd")
 public class DataConfiguration {
+	
+    private String driverClassName;
+    private String url;
+    private String username;
+    private String password;
+    private Database database;
+    private String databasePlatform;
 	
 	@Bean
     public DataSource dataSource(){
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/eventospringapp");
-        dataSource.setUsername("root");
-        dataSource.setPassword("root");
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(driverClassName);
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
         return dataSource;
     }
 	
 	@Bean
 	public JpaVendorAdapter jpaVendorAdapter(){
 		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
-		adapter.setDatabase(Database.MYSQL);
+		adapter.setDatabase(database);
 		adapter.setShowSql(true);
 		adapter.setGenerateDdl(true);
-		adapter.setDatabasePlatform("org.hibernate.dialect.MySQL8Dialect");
+		adapter.setDatabasePlatform(databasePlatform);
 		adapter.setPrepareConnection(true);
 		return adapter;
 	}
+
+	public String getDriverClassName() {
+		return driverClassName;
+	}
+
+	public void setDriverClassName(String driverClassName) {
+		this.driverClassName = driverClassName;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Database getDatabase() {
+		return database;
+	}
+
+	public void setDatabase(Database database) {
+		this.database = database;
+	}
+
+	public String getDatabasePlatform() {
+		return databasePlatform;
+	}
+
+	public void setDatabasePlatform(String databasePlatform) {
+		this.databasePlatform = databasePlatform;
+	}	
+	
 }
 
 
